528a59cbab72a5fbb57b63f517ea8a7fcfa401cc
kartikmangukiya <kmtech012@gmail.com>
2026-04-28 23:22:55 +0530

    frontend url added

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://your-chat-frontend.onrender.com/",
     credentials: true,
   }),
 );
